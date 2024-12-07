c6f7ff841a110f5d47392cfed0eaec22a35bdb30
Juan Villa <ing.villa.jimenez@gmail.com>
2024-12-06 22:57:28 -0600

    git-clone

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 ssh ubuntu@192.168.64.3 << EOF
   cd /home/ubuntu/server/jenkins2
 
-  git pull origin master
+  git clone https://github.com/ingvillajimenez/jenkins2
+  # git pull origin master
 EOF
